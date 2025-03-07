8af40f9e3a25131c5faeba44a76d8c26d9ff6e78
ndihoeric <endihokubwayo@asystresources.com>
2025-03-07 09:10:29 +0200

    sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    
+    <div className='sidebar'>
+      <ul>
+        <li><a href='#'>Dashboard</a></li>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>House</a></li>
+        <li><a href='#'>Tenant</a></li>
+        
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
