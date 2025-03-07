ce37118b9b92875f9976ed9330b24178977975c2
ndihoeric <endihokubwayo@asystresources.com>
2025-03-07 10:24:57 +0200

    sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import './globals.css'
 
 export default function Home() {
   return (
-    <>
+    <div className="container">
+    <Header/>
     <Sidebar/>
-    </>
+    </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../app/globals.css";
+
+function Header() {
+  return (
+    <div className='header'>
+        <ul>
+            <li><h3>Logo</h3></li>
+            <li><h3>EN</h3></li>
+            <li><h3>User</h3></li>
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Header
